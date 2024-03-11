d1f896fe6626b88665631867a4693ccfbf6cce56
iamt4jdd <chgdat10a3@gmail.com>
2024-03-11 12:31:30 +0700

    first commit

---
+++ src/test/java/com/example/muahexanh_resigtration/MuahexanhResigtrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.muahexanh_resigtration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuahexanhResigtrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
