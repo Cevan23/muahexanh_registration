72e7ffec1042ba971d5ff0851649b67243fb94ad
iamt4jdd <chgdat10a3@gmail.com>
2024-03-14 13:20:34 +0700

    Change User Entity to abstract

--- src/main/java/com/example/muahexanh_resigtration/entities/UserEntity.java
+++ src/main/java/com/example/muahexanh_resigtration/entities/UserEntity.java
@@ -1,12 +1,18 @@
 package com.example.muahexanh_resigtration.entities;
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
 import lombok.*;
 @Getter
 @Setter
 @NoArgsConstructor
-public class UserEntity {
+@MappedSuperclass
+public  abstract class UserEntity {
 
+    @Column(name = "username")
     private String username;
+    @Column(name = "password")
     private String password;
+    @Column(name = "role")
     private String role;
 
 }
