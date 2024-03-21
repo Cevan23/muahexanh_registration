4a5af9c0a40437a05d3ed21311992256429afa01
iamt4jdd <chgdat10a3@gmail.com>
2024-03-21 11:24:40 +0700

    Configure university response

--- src/main/java/com/example/muahexanh_resigtration/responses/University/UniversityResponse.java
+++ src/main/java/com/example/muahexanh_resigtration/responses/University/UniversityResponse.java
@@ -1,4 +1,44 @@
 package com.example.muahexanh_resigtration.responses.University;
 
+import com.example.muahexanh_resigtration.entities.StudentEntity;
+import com.example.muahexanh_resigtration.entities.UniversityEntity;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UniversityResponse {
+
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("full_name")
+    private String fullName;
+
+    @JsonProperty("phone_number")
+    private String phoneNumber;
+
+    @JsonProperty("role")
+    private String role;
+
+    @JsonProperty("university_name")
+    private String universityName;
+
+    public static com.example.muahexanh_resigtration.responses.University.UniversityResponse fromUniversity(UniversityEntity university) {
+        return com.example.muahexanh_resigtration.responses.University.UniversityResponse
+                .builder()
+                .id(university.getId())
+                .email(university.getEmail())
+                .fullName(university.getFullName())
+                .phoneNumber(university.getPhoneNumber())
+                .role(university.getRole())
+                .universityName(university.getUniversityName())
+                .build();
+    }
 }
